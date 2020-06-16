2cad193ac456361999d2db3f464ce21a43598847
Kyle G. Nally <kylegnally@hotmail.com>
2020-06-16 18:05:44 -0400

    Add project files.

---
+++ CSCI312_HashingExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI312_HashingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = null;
+
+            OurHashTable<Student> table = new OurHashTable<Student>();
+
+            // I've replaced the values given by the example file with two values
+            // I'd like to know the result of after they're handled.
+            for (int i = 100; i < 10000; i += 100)
+            {
+                s = new Student();
+                s.Id = (uint)(99999999);
+
+                table.Add((int)s.Id, s);
+            }
+
+            s = new Student();
+            s.Id = (uint)(99999899);
+
+            //for (int i = 100; i < 10000; i+= 100)
+            //{
+            //    s = new Student();
+            //    s.ID = (uint)(97+i);
+
+            //    table.Add((int)s.ID, s);
+            //}
+
+            //s = new Student();
+            //s.ID = (uint)(97 + 9800);
+
+            //table.Add((int)s.ID, s);
+
+            var z = table.Get(999997, new Student { Id = 197 });
+
+            s = new Student();
+            s.Id = (uint)(97 + 9900);
+
+            table.Add((int)s.Id, s);
+
+        }
+    }
+}
---
+++ CSCI312_HashingExample/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI312_HashingExample
+{
+    public class Student
+    {
+        private string _name;
+        private decimal _gpa;
+
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// Get/Set the name.  Throws exception on null assignment.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name; 
+            }
+            set
+            {
+                if (value != null)
+                    _name = value;
+                else
+                    throw new ArgumentNullException("Name cannot be null!");
+            }
+        }
+
+        public decimal GPA
+        {
+            get
+            {
+                return _gpa;
+            }
+            set
+            {
+                if (value >= 0.0m && value <= 4.0m)
+                    _gpa = value; 
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            string id = Id.ToString().PadLeft(8, '0');
+            id = id.Substring(6, 2);
+            return int.Parse(id);
+        }
+
+    }
+}
